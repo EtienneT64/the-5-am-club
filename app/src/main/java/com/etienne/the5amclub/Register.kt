701a116a952dca0c9af17a40aa0994dd4ff36144
ProgramKid1212 <81253061+ProgramKid1212@users.noreply.github.com>
2023-06-08 16:45:29 +0200

    Added User Model code

---
+++ app/src/main/java/com/etienne/the5amclub/Register.kt
@@ -0,0 +1,226 @@
+package com.etienne.the5amclub
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class Register : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var userRef: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
+        userRef = Firebase.database("https://the5amclub-dfb7f-default-rtdb.europe-west1.firebasedatabase.app").getReference("users")
+
+
+        setContent {
+            RegisterScreen()
+        }
+
+    }
+
+
+    private fun userCreation(email: String, password: String, firstname: String, starsign: String? = null) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success
+                    val user = Firebase.auth.currentUser
+                    val token = user?.getIdToken(false)
+                    //val username = user?.email.toString()
+                    Toast.makeText(
+                        baseContext,
+                        "$token!",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+
+                    //Adding User to Firebase Realtime Database
+                    //
+                    addUserToRealtime(firstname, email, starsign)
+
+                    Firebase.auth.signOut()
+
+                    val intent = Intent(this@Register, LogIn::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+            .addOnFailureListener{exception ->
+                Toast.makeText(
+                    this,
+                    exception.localizedMessage,
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+
+    }
+
+
+    private fun addUserToRealtime(firstname: String, email: String, starsign: String?){
+        val userID = userRef.push().key!!
+
+        val user = UserModel(userID, firstname, email, starsign)
+
+        userRef.child(userID).setValue(user)
+            .addOnCompleteListener{
+                Toast.makeText(
+                    baseContext,
+                    "User added!",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(
+                    this,
+                    exception.localizedMessage,
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun RegisterScreen() {
+        var inputFullName by remember {
+            mutableStateOf("")
+        }
+        var inputEmail by remember {
+            mutableStateOf("")
+        }
+        var inputPassword by remember{
+            mutableStateOf("")
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .offset(x = 0.dp, y = 100.dp),
+        ) {
+            Text(
+                text = "Sign Up",
+                fontSize = 50.sp,
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+
+            //Column to group the info buttons and labels
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(all = 10.dp)
+            ) {
+                Text(
+                    text = "First Name",
+                    color = Color.Blue,
+                    fontSize = 20.sp,
+                )
+                OutlinedTextField(
+                    value = inputFullName,
+                    onValueChange = { text ->
+                        inputFullName = text
+                    }
+                )
+                Text(
+                    text = "Email",
+                    color = Color.Blue,
+                    fontSize = 20.sp,
+                )
+                OutlinedTextField(
+                    value = inputEmail,
+                    onValueChange = { text ->
+                        inputEmail = text
+                    }
+                )
+                Text(
+                    text = "Password",
+                    color = Color.Blue,
+                    fontSize = 20.sp,
+                )
+                OutlinedTextField(
+                    value = inputPassword,
+                    onValueChange = {text ->
+                        inputPassword = text
+                    }
+                )
+            }
+            //Column to center the register button
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Button(onClick = {
+                    userCreation(inputEmail, inputPassword, inputFullName)
+                    inputEmail = ""
+                    inputPassword = ""
+                    inputFullName = ""
+                }) {
+                    Text(text="Register")
+                }
+                Button(onClick = {
+                    inputEmail = ""
+                    inputPassword = ""
+                    inputFullName = ""
+                    val intent = Intent(this@Register, LogIn::class.java)
+                    startActivity(intent)
+                }) {
+                    Text(text="Already have an account?")
+                }
+                Button(onClick = {
+                    //val intent = Intent(this@Register, BMICalculator::class.java)
+                    //startActivity(intent)
+                }) {
+                    Text(text="BMI Calculate")
+                }
+            }
+        }
+
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun RegisterPreview() {
+        RegisterScreen()
+    }
+}
--- app/src/main/java/com/etienne/the5amclub/UserModel.kt
+++ app/src/main/java/com/etienne/the5amclub/UserModel.kt
@@ -7,20 +7,107 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.tasks.await
 
-data class UserModel(var userID: String? = null,
-var userFullName: String? = null,
-var userEmail: String? = null,
-var userStarSign: String? = null,
-var userStatus: String? = null,
+class UserModel(
+    var userID: String? = null,
+    var userFullName: String? = null,
+    var userEmail: String? = null,
+    var userStarSign: String? = null,
+    var userStatus: String? =null,
 ){
-    fun getFullName(): String{
-        if (userFullName != null)
-        {
-        return userFullName.toString()
-        }
-        else{
-            return "null"
-        }
+    private lateinit var userRef: DatabaseReference
+    private lateinit var tempUser: UserModel
+
+    fun getUserObject(email: String): UserModel = runBlocking{
+        var userObject = getCurrentRealtimeUser(email)
+        return@runBlocking userObject
+
     }
+
+
+    private suspend fun getCurrentRealtimeUser(email: String): UserModel {
+        userRef =
+            Firebase.database("https://the5amclub-dfb7f-default-rtdb.europe-west1.firebasedatabase.app")
+                .getReference("users")
+
+        val snapshot = userRef.get().await()
+
+        for (userSnap in snapshot.children) {
+            tempUser = userSnap.getValue(UserModel::class.java)!!
+            Log.d("UserObject", tempUser.userEmail.toString())
+            if (tempUser.userEmail == email) {
+                Log.d("UserObject", "Found correct user.")
+                return tempUser
+            }
+        }
+
+        //return mapOf("userEmail" to "null")
+        Log.d("UserObject", "Could not find correct user.")
+        return UserModel()
+    }
+
+    //Using callbacks
+    /*fun getUserDetails(email: String, myCallback: UserCallback){
+        var userRef = Firebase.database("https://fir-auth-826d9-default-rtdb.europe-west1.firebasedatabase.app").getReference("users")
+        userRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()){
+                    for (userSnap in snapshot.children) {
+                        tempUser = userSnap.getValue(UserModel::class.java)!!
+                        Log.d("FB", tempUser?.userEmail!!)
+                        if (tempUser.userEmail == email) {
+                            Log.d("FB", "Found correct user.")
+                            myCallback.onCallback(tempUser.userEmail.toString())
+                            break
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("FB", "Find User cancelled.")
+            }
+
+        })
+    }*/
+
+    //Try using coroutines
+
+    //Using callbacks
+    /*fun getUserObject(email: String) {
+        getUserDetails(email, object: UserCallback{
+            override fun onCallback(value: String) {
+                UserModel.userEmail = value
+                Log.d("Callback", "This email is $value")
+            }
+        })
+
+    }
+
+    interface UserCallback{
+        fun onCallback(value: String)
+        }
+    }*/
+
+    /* @OptIn(ExperimentalCoroutinesApi::class)
+ fun getString(email: String, ExampleViewModel: ViewModel): String {
+         val result = ExampleViewModel.viewModelScope.async{
+             //var mappy = mapOf("userID" to "123")
+             var mappy = getCurrentRealtimeUser(email)
+             mappy
+         }
+
+     result.invokeOnCompletion {
+         if (it == null){
+             var userMap = result.getCompleted()
+
+             userEmail = userMap["userEmail"]
+         }
+     }
+
+     return userEmail.toString()
+
+ }*/
+
+
 }
 
