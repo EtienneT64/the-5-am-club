a59a0a20f1b8225599b450ed72c445dcf386a63f
ProgramKid1212 <81253061+ProgramKid1212@users.noreply.github.com>
2023-07-02 01:33:45 +0200

    Added new parameters to the usermodel class

--- app/src/main/java/com/etienne/the5amclub/UserModel.kt
+++ app/src/main/java/com/etienne/the5amclub/UserModel.kt
@@ -1,10 +1,14 @@
 package com.etienne.the5amclub
 
+import com.etienne.the5amclub.screens.Event
+
 data class UserModel(
     var userID: String? = null,
     var userFullName: String? = null,
     var userEmail: String? = null,
     var userStatus: String? = null,
+    var Attending: Map<String, String>? = null,
+    var Events: Map<String, Event>? = null,
 )
 
 
